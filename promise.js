f010aa02d9c1dfca6bfe126bddf84ffbc271a665
mnaauval <mnaauval20@gmail.com>
2022-06-11 10:33:39 +0700

    feat: promise

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emosi) => {
+  const dataIXX = await promiseTheaterIXX();
+  const dataVGC = await promiseTheaterVGC();
+  let countHasil = 0;
+  let allData = [...dataIXX, ...dataVGC];
+
+  allData.forEach((data) => {
+    if (data.hasil === emosi) {
+      countHasil++;
+    }
+  });
+
+  return countHasil;
+};
 
 module.exports = {
   promiseOutput,
